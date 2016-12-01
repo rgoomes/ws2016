bbc3f78286164d0479720d097b34623c94092b2c
rgoomes <ricardo.jpg@hotmail.com>
2016-12-01 16:26:01 +0000

    fixed whitespace bug in sparql queries. new sparql query to get entities. find records and artists that match their tracks titles

--- server/sparql/KeywordSearch.java
+++ server/sparql/KeywordSearch.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
@@ -12,55 +14,65 @@ public class KeywordSearch extends Object {
 
 	static Scanner sc = new Scanner(System.in);
 	static Model model = ModelFactory.createDefaultModel();
-	
-	public static void run_query(String query, String column){
+
+	public static void run_query(String query, String[] columns){
+		if(query == null)
+			return;
+
 		Query qry = QueryFactory.create(query);
 		QueryExecution qe = QueryExecutionFactory.create(qry, model);
 		ResultSet rs = qe.execSelect();
-		
+
 		while(rs.hasNext()) {
 			QuerySolution sol = rs.nextSolution();
-			RDFNode s = sol.get(column);
-			System.out.println(s.toString());
+
+			for(String col : columns){
+				RDFNode node = sol.get(col);
+				System.out.print(node.toString() + "\t");
+			}
+
+			System.out.println("");
 		}
 	}
 
 	public static void parse_input(String[] keywords) {
-		String search_keywords = "";
-		for(int i = 1; i < keywords.length - 1; i++)
-			search_keywords += keywords[i] + (i == keywords.length - 2 ? "" : " ");
+		if(keywords.length < 2)
+			return;
 
 		String limit = keywords[keywords.length - 1];
+		String[] slice = Arrays.copyOfRange(keywords, 1, keywords.length - 1);
+		String search_keywords = Arrays.stream(slice).collect(Collectors.joining(" ")).toLowerCase();
 
 		if(keywords[0].equals("tracks")){
 			String query = QueryGenerator.tracksQuery(search_keywords, limit);
-			run_query(query, "track");
+			run_query(query, new String[] {"track"});
 		}
 		else if(keywords[0].equals("artists")){
 			String query = QueryGenerator.artistsQuery(search_keywords, limit);
-			run_query(query, "artist");
+			run_query(query, new String[] {"artist"});
 		}
 		else if(keywords[0].equals("records")){
 			String query = QueryGenerator.recordsQuery(search_keywords, limit);
-			run_query(query, "record");
+			run_query(query, new String[] {"record"});
 		}
 		else if(keywords[0].equals("genres")){
 			String query = QueryGenerator.genresQuery(search_keywords, limit);
-			run_query(query, "record");
+			run_query(query, new String[] {"record"});
 		}
-		else {
-			
+		else if(keywords[0].equals("track") || keywords[0].equals("artist") || keywords[0].equals("record")){
+			String query = QueryGenerator.getEntity(search_keywords + limit);
+			run_query(query, new String[] {"predicate", "object"});
 		}
 	}
 
 	public static void main(String args[]) {
 		model.read("clean_jamendo.ttl", "TURTLE") ;
-		
+
 		while(true) {
 			System.out.print(">>> ");
 			String req = sc.nextLine();
 			String[] words = req.split(" ");
-			
+
 			try {
 				parse_input(words);
 			} catch(Exception e){}
--- server/sparql/QueryGenerator.java
+++ server/sparql/QueryGenerator.java
@@ -1,18 +1,27 @@
+import java.util.regex.*;
 
 public class QueryGenerator {
-	
+
 	public static String quote(String str){
 		return "\"" + str + "\"";
 	}
-	
+
+	public static String tag(String str){
+		return "<" + str + ">";
+	}
+
+	public static String getEntity(String keyword){
+		return "SELECT * " + "WHERE { " + tag(keyword) + " ?predicate ?object } ";
+	}
+
 	public static String tracksQuery(String keyword, String limit) {
 		return
-			"SELECT ?track " +
+			"SELECT DISTINCT ?track " +
 			"WHERE {{ " +
 				"?track <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/Track> . " +
 				"?track <http://purl.org/dc/elements/1.1/title> ?title FILTER regex(LCASE(str(?title)), " + quote(keyword) + ") . " +
 			"} " +
-			"UNION { " + 
+			"UNION { " +
 				"?artist <http://xmlns.com/foaf/0.1/name> " + quote(keyword) + " . " +
 				"?artist <http://xmlns.com/foaf/0.1/made> ?record . " +
 				"?record <http://purl.org/ontology/mo/track> ?track . " +
@@ -22,20 +31,37 @@ public class QueryGenerator {
 				"?lyrics <http://purl.org/ontology/mo/text> ?text FILTER regex(LCASE(str(?text)), " + quote(keyword) + ") . " +
 				"?description <http://purl.org/ontology/mo/recorded_as> ?signal . " +
 				"?signal <http://purl.org/ontology/mo/published_as> ?track . " +
-			"}} " + 
+			"}} " +
 			(limit.equals("inf") ? "" : "LIMIT " + limit);
 	}
-	
+
 	public static String recordsQuery(String keyword, String limit) {
-		return
-			"SELECT DISTINCT ?record " + 
+		String query =
+			"SELECT DISTINCT ?record " +
 			"WHERE {{ " +
 				"?record <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/Record> . " +
 				"?record <http://purl.org/dc/elements/1.1/title> ?title FILTER regex(LCASE(str(?tile)), " + quote(keyword) + ") . " +
-			"} " +
-			"UNION { " + 
+			"} ";
+
+		Pattern pattern = Pattern.compile("\\s");
+		Matcher matcher = pattern.matcher(keyword);
+		boolean whitespacesFound = matcher.find();
+
+		// whitespaces are not valid in fields that are not of the type string
+		// without this sparql complains and returns an exception
+		if(!whitespacesFound){
+			query +=
+				"UNION { " +
+					"?record <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/Record> . " +
+					"?record <http://www.holygoat.co.uk/owl/redwood/0.1/tags/taggedWithTag> " + tag("http://dbtune.org/jamendo/tag/" + keyword) + " . " +
+				"} ";
+		}
+
+		query +=
+			"UNION { " +
 				"?record <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/Record> . " +
-				"?record <http://www.holygoat.co.uk/owl/redwood/0.1/tags/taggedWithTag> " + "<http://dbtune.org/jamendo/tag/" + keyword + "> . " +
+				"?record <http://purl.org/ontology/mo/track> ?track . " +
+				"?track  <http://purl.org/dc/elements/1.1/title> ?title FILTER regex(LCASE(str(?title)), " + quote(keyword) + ") . " +
 			"} " +
 			"UNION { " +
 				"?record <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/Record> . " +
@@ -43,34 +69,64 @@ public class QueryGenerator {
 				"?artist <http://xmlns.com/foaf/0.1/name> ?name FILTER regex(LCASE(str(?name)), " + quote(keyword) + ") . " +
 			"}} " +
 			(limit.equals("inf") ? "" : "LIMIT " + limit);
+
+		return query;
 	}
-	
+
 	public static String genresQuery(String keyword, String limit) {
-		return
-			"SELECT DISTINCT ?record " +
-			"WHERE { " +
-				"?record <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/Record> . " +
-				"?record <http://www.holygoat.co.uk/owl/redwood/0.1/tags/taggedWithTag> <http://dbtune.org/jamendo/tag/" + keyword + "> . " +
-			"} " + 
-			(limit.equals("inf") ? "" : "LIMIT " + limit);
+		Pattern pattern = Pattern.compile("\\s");
+		Matcher matcher = pattern.matcher(keyword);
+		boolean whitespacesFound = matcher.find();
+
+		if(!whitespacesFound){
+			return
+				"SELECT DISTINCT ?record " +
+				"WHERE { " +
+					"?record <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/Record> . " +
+					"?record <http://www.holygoat.co.uk/owl/redwood/0.1/tags/taggedWithTag> " + tag("http://dbtune.org/jamendo/tag/" + keyword) + " . " +
+				"} " +
+				(limit.equals("inf") ? "" : "LIMIT " + limit);
+		}
+
+		return null;
 	}
-	
+
 	public static String artistsQuery(String keyword, String limit) {
-		return
-			"SELECT DISTINCT ?artist " + 
+		String query =
+			"SELECT DISTINCT ?artist " +
 			"WHERE {{ " +
 				"?artist <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/MusicArtist> . " +
-				"?artist <http://xmlns.com/foaf/0.1/name> " + quote(keyword) + " . " + 
-			"} " + 
-			"UNION { " + 
-				"?artist <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/MusicArtist> . " + 
-				"?artist <http://xmlns.com/foaf/0.1/homepage> <" + keyword +  "> . " +
+				"?artist <http://xmlns.com/foaf/0.1/name> ?name FILTER regex(LCASE(str(?name)), " + quote(keyword) + ") . " +
+			"} ";
+
+		Pattern pattern = Pattern.compile("\\s");
+		Matcher matcher = pattern.matcher(keyword);
+		boolean whitespacesFound = matcher.find();
+
+		// whitespaces are not valid in fields that are not of the type string
+		// without this sparql complains and returns an exception
+		if(!whitespacesFound){
+			query +=
+				"UNION { " +
+					"?artist <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/MusicArtist> . " +
+					"?artist <http://xmlns.com/foaf/0.1/homepage> " + tag(keyword) + " . " +
+				"} ";
+		}
+
+		query +=
+			"UNION { " +
+				"?artist <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/MusicArtist> . " +
+				"?artist <http://xmlns.com/foaf/0.1/made> ?record . " +
+				"?record <http://purl.org/ontology/mo/track> ?track . " +
+				"?track  <http://purl.org/dc/elements/1.1/title> ?title FILTER regex(LCASE(str(?title)), " + quote(keyword) + ") . " +
 			"} " +
-			"UNION { " + 
+			"UNION { " +
 				"?artist <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/mo/MusicArtist> . " +
 				"?artist <http://purl.org/ontology/mo/biography> ?biography FILTER regex(str(?biography), " + quote(keyword) + ") . " +
 			"}} " +
 			(limit.equals("inf") ? "" : "LIMIT " + limit);
+
+		return query;
 	}
-	
+
 }
